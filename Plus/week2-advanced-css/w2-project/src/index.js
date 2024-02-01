4aca94e6c5afa5cde701450e5cca75348968ad73
Khadijah Taherah <k.taherah@gmail.com>
2024-02-01 17:55:20 +1000

    Created object array of cities

---
+++ Plus/week2-advanced-css/w2-project/src/index.js
@@ -0,0 +1,34 @@
+let weather = [
+  {
+    city: "paris",
+    temp: 19.7,
+    humidity: 80,
+  },
+  {
+    city: "tokyo",
+    temp: 17.3,
+    humidity: 50,
+  },
+  {
+    city: "lisbon",
+    temp: 30.2,
+    humidity: 20,
+  },
+  {
+    city: "san francisco",
+    temp: 20.9,
+    humidity: 100,
+  },
+  {
+    city: "oslo",
+    temp: -5,
+    humidity: 20,
+  },
+];
+
+// write your code here
+//first add button selector
+//lowercase input
+//compare input with array element
+//if matches -> alert output data
+//if not -> alert link info
