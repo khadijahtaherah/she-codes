0d3cafa98f6cd697a16ffcbdec12f9c2ffbffcea
Khadijah Taherah <k.taherah@gmail.com>
2024-02-01 14:22:12 +1000

    Add JS Objects challenge files

---
+++ Plus/week3-JS-datatypes/lesson6-JS-object/src/index.js
@@ -0,0 +1,17 @@
+//JS Challenge 1
+//Create an object weather and console log it
+
+//JS Challenge 2
+//Create an object weather with properties of temp and humidity and console log the object
+
+//JS Challenge 3
+//console log the humidity and the temperature
+
+//JS Challenge 4
+//Add a property windSpeed and console log it
+
+//JS Challenge 5
+//Console log the wind speed, the temperature and the humidity using the square brackets syntax
+
+//JS Challenge 6
+//Create an object or an array of objects called `forecast` with each day’s temperature, and console log i
